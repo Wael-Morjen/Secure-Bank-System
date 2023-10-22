7be1106a8e5cbfccae1e789591fa10e1eaf896ef
Wael-Morjen <waelmorjen@gmail.com>
2023-10-22 21:20:22 +0100

    Credit & Debit request class

--- src/main/java/com/project/banksystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/banksystem/service/impl/UserServiceImpl.java
@@ -101,4 +101,31 @@ public class UserServiceImpl implements UserService {
         return  foundUser.getFirstName() + " " + foundUser.getLastName();
     }
 
+    @Override
+    public BankResponse creditAccount(CreditDebitRequest request) {
+        // checking if the account exist
+        boolean isAccountExist = userRepository.existsByAccountNumber(request.getAccountNumber());
+        if (!isAccountExist) {
+            return BankResponse.builder()
+                    .responseCode(AccountUtils.ACCOUNT_NOT_EXIST_CODE)
+                    .responseMessage(AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE)
+                    .accountInfo(null)
+                    .build();
+        }
+
+        User userToCredit = userRepository.findByAccountNumber(request.getAccountNumber());
+        userToCredit.setAccountBalance(userToCredit.getAccountBalance().add(request.getAmount()));
+        userRepository.save(userToCredit);
+        return BankResponse.builder()
+                .responseCode(AccountUtils.ACCOUNT_CREDIT_SUCCESS_CODE)
+                .responseMessage(AccountUtils.ACCOUNT_CREDIT_SUCCESS_MESSAGE)
+                .accountInfo(AccountInfo.builder()
+                        .accountName(userToCredit.getFirstName() + " " + userToCredit.getLastName())
+                        .accountBalance(userToCredit.getAccountBalance())
+                        .accountNumber(request.getAccountNumber())
+                        .build())
+                .build();
+
+    }
+
 }
