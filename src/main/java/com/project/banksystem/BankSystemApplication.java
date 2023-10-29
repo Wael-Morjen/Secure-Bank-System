7fb4f63ae6ca462d1c32a9bb7f413f979a014e24
Wael-Morjen <waelmorjen@gmail.com>
2023-10-29 18:29:00 +0100

    Swagger-ui integration

--- src/main/java/com/project/banksystem/BankSystemApplication.java
+++ src/main/java/com/project/banksystem/BankSystemApplication.java
@@ -1,9 +1,13 @@
 package com.project.banksystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+
+)
 public class BankSystemApplication {
 
     public static void main(String[] args) {
