9cfc6611a0db1f8281db3bdbcdf45fc53ef2efd1
Wael-Morjen <waelmorjen@gmail.com>
2023-11-01 19:22:13 +0100

    Swagger-ui DocInfo

--- src/main/java/com/project/banksystem/BankSystemApplication.java
+++ src/main/java/com/project/banksystem/BankSystemApplication.java
@@ -1,12 +1,33 @@
 package com.project.banksystem;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-
+    info = @Info(
+            title = "MyBank Application",
+            description = "Backend REST APIs for MyBank",
+            version = "v1.0",
+            contact = @Contact(
+                    name = "Wael Morjen & Malek Bougadouha",
+                    email = "waelmorjen@gmail.com",
+                    url = "https://github.com/Wael-Morjen/Secure-Bank-System"
+            ),
+            license = @License(
+                    name = "MyBank Project",
+                    url = "https://github.com/Wael-Morjen/Secure-Bank-System"
+            )
+    ),
+    externalDocs = @ExternalDocumentation(
+            description = "MyBank App Documentation",
+            url = "https://github.com/Wael-Morjen/Secure-Bank-System"
+    )
 )
 public class BankSystemApplication {
 
--- src/main/java/com/project/banksystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/banksystem/service/impl/UserServiceImpl.java
@@ -11,7 +11,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 @Service
-
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
