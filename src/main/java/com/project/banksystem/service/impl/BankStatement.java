655ecfe5c4269170696bfe01e45e9399557c5f61
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 19:57:50 +0100

    Crete transaction table

--- src/main/java/com/project/banksystem/service/impl/BankStatement.java
+++ src/main/java/com/project/banksystem/service/impl/BankStatement.java
@@ -1,26 +1,39 @@
 package com.project.banksystem.service.impl;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.project.banksystem.entity.Transaction;
+import com.project.banksystem.entity.User;
 import com.project.banksystem.repository.TransactionRepository;
+import com.project.banksystem.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class BankStatement {
 
     private TransactionRepository transactionRepository;
+    private UserRepository userRepository;
+    private static final String FILE = "home\\morgan\\Documents\\MyBankStatement.pdf";
     /*
      *  retrieve list of transactions within a date range given an account number
      *  generate a PDF file of transactions
      * send the file via email
      */
 
-    public List<Transaction> generateBankStatement(String accountNumber, String startDate, String endDate) {
+    public List<Transaction> generateBankStatement(String accountNumber, String startDate, String endDate) throws DocumentException, FileNotFoundException {
         LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
         LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
 
@@ -29,10 +42,75 @@ public class BankStatement {
                 .filter(transaction -> transaction.getCreatedAt().isEqual(start))
                 .filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
 
+        User user = userRepository.findByAccountNumber(accountNumber);
+        String customerName = user.getFirstName() + " " + user.getLastName();
+
+        Rectangle statementSize = new Rectangle(PageSize.A4);
+        Document document = new Document(statementSize);
+        log.info("setting size of the document");
+        OutputStream outputStream = new FileOutputStream(FILE);
+        PdfWriter.getInstance(document, outputStream);
+        document.open();
+
+        PdfPTable bankInfoTable = new PdfPTable(1);
+        PdfPCell bankName = new PdfPCell(new Phrase("Native Spring Bank"));
+        bankName.setBorder(0);
+        bankName.setBackgroundColor(BaseColor.GREEN);
+        bankName.setPadding(20f);
+
+        PdfPCell bankAddress = new PdfPCell(new Phrase("41, Kantaoui, Hammam Sousse, Sousse 4089, Tunisia"));
+        bankAddress.setBorder(0);
+        bankInfoTable.addCell(bankName);
+        bankInfoTable.addCell(bankAddress);
+
+        PdfPTable statementInfo = new PdfPTable(2);
+        PdfPCell customerInfo = new PdfPCell(new Phrase( "Start Date: " + startDate));
+        customerInfo.setBorder(0);
+        PdfPCell statement = new PdfPCell(new Phrase("STATEMENT OF ACCOUNT"));
+        statement.setBorder(0);
+        PdfPCell stopDate = new PdfPCell(new Phrase("End Date" + endDate));
+        stopDate.setBorder(0);
+        PdfPCell name = new PdfPCell(new Phrase("Customer Name: " + customerName));
+        name.setBorder(0);
+        PdfPCell space = new PdfPCell();
+        space.setBorder(0);
+        PdfPCell address = new PdfPCell(new Phrase("Customer Address: " + user.getAddress()));
+        address.setBorder(0);
+
+        PdfPTable transactionsTable = new PdfPTable(4);
+        PdfPCell date = new PdfPCell(new Phrase("DATE"));
+        date.setBackgroundColor(BaseColor.GREEN);
+        date.setBorder(0);
+        PdfPCell transactionType = new PdfPCell(new Phrase("TRANSACTION TYPE"));
+        transactionType.setBackgroundColor(BaseColor.GREEN);
+        transactionType.setBorder(0);
+        PdfPCell transactionAmount = new PdfPCell(new Phrase("TRANSACTION AMOUNT"));
+        transactionAmount.setBackgroundColor(BaseColor.GREEN);
+        transactionAmount.setBorder(0);
+        PdfPCell status = new PdfPCell(new Phrase("STATUS"));
+        status.setBackgroundColor(BaseColor.GREEN);
+        status.setBorder(0);
+
+        transactionsTable.addCell(date);
+        transactionsTable.addCell(transactionType);
+        transactionsTable.addCell(transactionAmount);
+        transactionsTable.addCell(status);
+
+        transactionList.forEach(transaction -> {
+            transactionsTable.addCell(new Phrase(transaction.getCreatedAt().toString()));
+            transactionsTable.addCell(new Phrase(transaction.getTransactionType()));
+            transactionsTable.addCell(new Phrase(transaction.getAmount().toString()));
+            transactionsTable.addCell(new Phrase(transaction.getStatus()));
+        });
+
+        statementInfo.addCell(customerInfo);
+        statementInfo.addCell(statement);
+        statementInfo.addCell(endDate);
+        statementInfo.addCell(name);
+        statementInfo.addCell(space);
+        statementInfo.addCell(address);
+
         return transactionList;
     }
 
-    private void designStatement(List<Transaction> transactions) {
-
-    }
 }
