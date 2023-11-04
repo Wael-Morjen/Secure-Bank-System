21c14699fbec37ca32b90ad3240956dedcb5c1bc
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 10:39:26 +0100

    Transaction DTO

---
+++ src/main/java/com/project/banksystem/dto/TransactionDto.java
@@ -0,0 +1,25 @@
+package com.project.banksystem.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TransactionDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String transactionId;
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String status;
+}
