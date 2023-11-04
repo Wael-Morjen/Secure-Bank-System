a7be85dc60f93f75b1acdd264bc0824836c29b9d
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 10:39:19 +0100

    Transaction class

---
+++ src/main/java/com/project/banksystem/entity/Transaction.java
@@ -0,0 +1,24 @@
+package com.project.banksystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String transactionId;
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String status;
+}
