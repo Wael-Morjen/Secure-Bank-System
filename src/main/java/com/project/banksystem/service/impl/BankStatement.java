3facb9771d0945f03e41a8d75730995b48358ddd
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 17:40:33 +0100

    bank statement service

--- src/main/java/com/project/banksystem/service/impl/BankStatement.java
+++ src/main/java/com/project/banksystem/service/impl/BankStatement.java
@@ -5,6 +5,8 @@ import com.project.banksystem.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -18,9 +20,19 @@ public class BankStatement {
      * send the file via email
      */
 
-    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) {
+    public List<Transaction> generateBankStatement(String accountNumber, String startDate, String endDate) {
+        LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+        LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+
         List<Transaction> transactionList = transactionRepository.findAll().stream()
                 .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> transaction.get)
+                .filter(transaction -> transaction.getCreatedAt().isEqual(start))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
+
+        return transactionList;
+    }
+
+    private void designStatement(List<Transaction> transactions) {
+
     }
 }
