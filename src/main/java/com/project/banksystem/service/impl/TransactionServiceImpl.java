84ca4342d339aad003f1939f2c44e5a861538680
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 10:39:51 +0100

    Transaction service implementation

---
+++ src/main/java/com/project/banksystem/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,24 @@
+package com.project.banksystem.service.impl;
+
+import com.project.banksystem.dto.TransactionDto;
+import com.project.banksystem.entity.Transaction;
+import com.project.banksystem.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+    @Autowired
+     TransactionRepository transactionRepository;
+    @Override
+    public void saveTransaction(TransactionDto transactionDto) {
+        Transaction transaction = Transaction.builder()
+                .transactionType(transactionDto.getTransactionType())
+                .accountNumber((transactionDto.getAccountNumber()))
+                .amount(transactionDto.getAmount())
+                .status("SUCCESS")
+                .build();
+        transactionRepository.save(transaction);
+        System.out.println("Transaction saved successfully");
+    }
+}
