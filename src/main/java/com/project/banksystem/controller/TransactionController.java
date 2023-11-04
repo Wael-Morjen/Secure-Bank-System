48e70653d30716898a86273af67abd70b0a27142
Wael-Morjen <waelmorjen@gmail.com>
2023-11-04 17:40:13 +0100

    get all transactions Method

---
+++ src/main/java/com/project/banksystem/controller/TransactionController.java
@@ -0,0 +1,26 @@
+package com.project.banksystem.controller;
+
+import com.project.banksystem.entity.Transaction;
+import com.project.banksystem.service.impl.BankStatement;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bankStatement")
+@AllArgsConstructor
+public class TransactionController {
+
+    private BankStatement bankStatement;
+
+    @GetMapping
+    public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
+                                                   @RequestParam String startDate,
+                                                   @RequestParam String endDate) {
+        return bankStatement.generateBankStatement(accountNumber, startDate, endDate);
+    }
+}
