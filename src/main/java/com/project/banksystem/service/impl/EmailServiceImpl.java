b8b5035f5b7dada7d4ef2f282f2d61b44b0105b0
Wael-Morjen <waelmorjen@gmail.com>
2023-11-05 16:44:45 +0100

    implement emailPdf method

--- src/main/java/com/project/banksystem/service/impl/EmailServiceImpl.java
+++ src/main/java/com/project/banksystem/service/impl/EmailServiceImpl.java
@@ -1,14 +1,22 @@
 package com.project.banksystem.service.impl;
 
 import com.project.banksystem.dto.EmailDetails;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.util.Objects;
+
 @Service
+@Slf4j
 public class EmailServiceImpl implements EmailService{
 
     @Autowired
@@ -32,6 +40,27 @@ public class EmailServiceImpl implements EmailService{
             javaMailSender.send(mailMessage);
             System.out.println("Mail sent Successfully");
         } catch (MailException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void sendEmailWithPdf(EmailDetails emailDetails) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+        try {
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(senderEmail);
+            mimeMessageHelper.setTo(emailDetails.getRecipient());
+            mimeMessageHelper.setText(emailDetails.getMessageBody());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
+            mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+            javaMailSender.send(mimeMessage);
+
+            log.info(file.getFilename() + " has been sent to user with email " + emailDetails.getRecipient());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
