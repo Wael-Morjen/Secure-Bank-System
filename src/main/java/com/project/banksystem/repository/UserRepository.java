a603e6ae133fe6d865f845912267edc9848a4b80
Wael-Morjen <waelmorjen@gmail.com>
2023-11-18 22:36:02 +0100

    find by email method

--- src/main/java/com/project/banksystem/repository/UserRepository.java
+++ src/main/java/com/project/banksystem/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.project.banksystem.repository;
 import com.project.banksystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
     Boolean existsByAccountNumber(String accountNumber);
     User findByAccountNumber(String accountNumber);
 }
